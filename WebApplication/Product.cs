98976b3fe1a80c946d3f1f4ac94b47a8aebf12ef
pavlo <pavlo@DESKTOP-EL2P8T0>
2023-02-04 21:30:50 +0200

    add prodcut endpoints

--- WebApplication/Product.cs
+++ WebApplication/Product.cs
@@ -5,12 +5,5 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public int Id { get; set; }
-
-        public Product(string name, string desc, int id)
-        {
-            Name = name;
-            Description = desc;
-            Id = id;
-        }
     }
 }
