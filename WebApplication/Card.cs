72cfd0a0f29d366ef5eb11912b6ddc5ccfa0d148
Misha Andrytskyi <phantomassassin933@gmail.com>
2023-02-05 23:57:12 +0200

    add Gallery Controller

---
+++ WebApplication/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using WebApplication.Controllers;
+
+namespace WebApplication
+{
+    public class Card 
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Int64 Date { get; set; }
+    }
+}
